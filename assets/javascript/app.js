ea6a8acd0c6bf92f52fd07c64987d9d7a56abf0e
mapachurro <oliver.renwick@gmail.com>
2018-11-29 20:28:40 -0500

    backend basically built

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,54 @@
 
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
+
+
+// variables
+
+
+
+
+
 function searchArticles() {
-
-  var searchTerm = $(this).attr("data-name");
-  var queryURL = "https://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
-
+    
   // Creating an AJAX call for the specific article search term
-    $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?$.param({dcefdbf518e9494aa9bb2ec0d87c9bbf" + "?q=" + searchTerm + "?begin_date=" + beginDate + "?end_date=" + endDate + "?page=" + numResults + "})";
+  
+  var beginDate = "20100115"; 
+    
+  var endDate = "20180115";
+    
+  var numResults = "8";
+    
+  var searchTerm = "healthcare";
+
+  $.ajax({
+    url: queryURL, 
+    method: 'GET',
+  }).done(function(result) {
+    console.log(result);
+  }).fail(function(err) {
+    throw err;
+  });
+
+}
+
+//function call
+searchArticles();
+
+
+// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+// url += '?' + $.param({
+//   'api-key': "dcefdbf518e9494aa9bb2ec0d87c9bbf",
+//   'q': "Manafort",
+//   'begin_date': "20100115",
+//   'end_date': "20180115",
+//   'page': 5
+// });
+// $.ajax({
+//   url: url,
+//   method: 'GET',
+// }).done(function(result) {
+//   console.log(result);
+// }).fail(function(err) {
+//   throw err;
+// });
